7a46a74c1a7ec86e7c765b2112f35d16b004881c
rascalnoshippo <naofirefox@gmail.com>
2023-01-04 22:28:36 +0900

    テスト

--- テスト/1.js
+++
@@ -1,6 +0,0 @@
-var sum =0;
-for(let i=1;i<=12343;i+=3){
-  sum += i*(i+1)/(i+2);
-}
-
-console.log(sum);
--- テスト/2.js
+++
@@ -1,9 +0,0 @@
-var sum = 0;
-
-for(let i=1; i<=1024; i++){
-  for(let j=1 ;j<=1024 ;j++){
-    sum += i % j;
-  }
-}
-
-console.log(sum)
